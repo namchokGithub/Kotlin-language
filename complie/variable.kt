2a629cf784ec4133a6a7d08bc98b00159b1deaf4
namchokGithub <pech4751@gmail.com>
2019-12-07 13:17:48 +0700

    edit varible

--- complie/variable.kt
+++ complie/variable.kt
@@ -2,9 +2,9 @@ fun main(args: Array<String>) {
     var name: String
     val age: Int
 
-    name = "Namchok"
+    name = "Namchok" // immu
     age = 12
-    age = 13
+    // age = 13 // mu
 
     println("My name ${name} I am ${age}")
 }

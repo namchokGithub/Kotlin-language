74d4bf0181232a04606f4e126239407dcac284ba
namchokGithub <pech4751@gmail.com>
2019-12-08 18:14:24 +0700

    if statement

---
+++ KongRuksiam/ifStatement.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    var number=0
+
+    if(number>=0){
+        println("Positive")
+    }else if(number<0){
+        println("Negative")
+    }else{
+        println("Not number")
+    }
+}

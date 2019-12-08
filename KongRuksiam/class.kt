8d64790215bf1d37a1128d1caa6f88ac13945c60
namchokGithub <pech4751@gmail.com>
2019-12-08 17:46:32 +0700

    class

---
+++ KongRuksiam/class.kt
@@ -0,0 +1,23 @@
+class Product{
+    public var name: String 
+    public var price: Int
+
+    constructor(){
+        this.name = ""
+        this.price = 0
+    }
+    constructor(name: String, price: Int){
+        this.name = name
+        this.price = price
+    }
+
+    public fun product_show(){
+        println("Name: ${this.name} Price: ${this.price}")
+    }
+
+}
+
+fun main(args: Array<String>) {
+    val p1 = Product("ball", 100)
+    p1.product_show();
+}

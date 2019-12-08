1d6ab5ce27d20ca849a366e981d06f14958d28df
namchokGithub <pech4751@gmail.com>
2019-12-08 12:38:06 +0700

    var and val

---
+++ KongRuksiam/VarAndVal.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    var var1="String1" // Mutable เปลี่ยนแปลงค่าได้
+    val var2="String2" // Immutable เปลี่ยนแปลงค่าไม่ได้
+
+    println(var1)
+    println(var2)
+}
